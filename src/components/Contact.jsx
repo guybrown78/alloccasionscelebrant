84fd79388c5b400b0a10983cd915c17c050ffc11
Guy Brown <guy.brown@justchecking.co.uk>
2023-09-25 14:26:38 +0100

    added contact details

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,38 @@ export function Contact(){
 				<div className="py-24 sm:py-32">
 					<div className="mx-auto max-w-7xl px-6 lg:px-8">
 			
+						<div className="mx-auto lg:mx-0">
+							
+							<h2 className="text-3xl font-light tracking-tight text-aoc-navy-dark sm:text-4xl">Get in Touch</h2>
 
+							<p>
+								Do you have any questions or are you ready to start organising your unforgettable occasion? Let's make your celebration, your vision, a reality. Contact today and create something truly special. We are based in Middlesbrough and cover all of Teesside, Tees Valley and North Yorkshire.
+							</p>
+
+							<div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-lg font-light leading-8 text-aoc-navy-dark lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
+				
+								<div>
+									<h4 className="mb-2 text-md font-medium leading-4 text-slate-600">Email</h4>
+									<a href="mailto:caroline@alloccasionscelebrant.com" className="text-md leading-4 text-slate-600 hover:text-slate-900 hover:underline">
+										caroline@alloccasionscelebrant.com
+									</a>
+								</div>
+								<div>
+									<h4 className="mb-2 text-md font-medium leading-4 text-slate-600">Telephone</h4>
+									<a href="tel:+447843 469738" className="text-md leading-4 text-slate-600 hover:text-slate-900 hover:underline">
+										07843 469738
+									</a>
+								</div>
+								<div>
+									<h4 className="mb-2 text-md font-medium leading-4 text-slate-600">Location</h4>
+									<p className="text-md leading-6 text-slate-600">
+										Nunthorpe,<br />Middlesbrough,<br />Teesside
+									</p>
+								</div>
+							
+						</div>
+
+				</div>
 					</div>
 				</div>
 			</Container>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ export function Footer() {
             </p>
             <div className="flex space-x-6">
 
-						<Link	
+						{/* <Link	
 									href="https://www.tiktok.com/@all_occasions_cel"
 									className="group"
 									aria-label="All Occasions Celebrant on TikTok"
@@ -57,9 +57,9 @@ export function Footer() {
 									>
 										<path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
 									</svg>
-								</Link>
+								</Link> */}
 								<Link
-									href="https://www.instagram.com/all_occasions_celebrant"
+									href="https://www.instagram.com/all_occasions_celebrant/"
 									className="group"
 									aria-label="All Occasions Celebrant on Instagram"
 								>
@@ -71,7 +71,7 @@ export function Footer() {
 									</svg>
 								</Link>
 								<Link
-									href="https://www.facebook.com/alloccasionscelebrantservices/"
+									href="https://www.facebook.com/profile.php?id=61550956029548"
 									className="group"
 									aria-label="All Occasions Celebrant on facebook"
 								>
@@ -83,7 +83,7 @@ export function Footer() {
 									</svg>
 								</Link>
 								<Link
-									href="https://linkedin.com/company/alloccasionscelebrant"
+									href="https://www.linkedin.com/in/caroline-brown-aaa117293/"
 									className="group"
 									aria-label="All Occasions Celebrant on LinkedIn"
 								>
